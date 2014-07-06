f7a2543b78a3177b4d62ab6c621e4a02534b881f
~/.gitconfig <benmiller179@gmail.com>
2014-07-06 15:39:22 -0400

    Pull out remaining layout items into InviteLayout

--- app/views/invite_layout.rb
+++ app/views/invite_layout.rb
@@ -44,5 +44,59 @@ class InviteLayout < MK::Layout
     @map = add MapKit::MapView, :map do
       frame [[0,284],['100%',239]]
     end
+    
+    @button_accept = add UIButton, :button_accept do
+      background_color UIColor.colorWithRed(0.118, green:0.541, blue:0.545, alpha:1.0)
+      title "Swipe to accept >"
+      sizeToFit
+      frame [[0,523],['100%',45]]
+    end
+
+    @label_time = add UILabel, :label_time do
+      background_color UIColor.whiteColor
+      text "10:54"
+      frame [[0,245],['100%',40]]
+    end
+    @label_time.tap do |l|
+      l.textAlignment = UIControlContentHorizontalAlignmentLeft
+      l.font = UIFont.fontWithName("Helvetica-Bold", size: 16)
+    end
+
+    @image_calendar = UIImageView.alloc.initWithImage(UIImage.imageNamed("calendar4.png"))
+    addSubview(@image_calendar)
+    @image_calendar.center = CGPointMake(80, 130)
+
+    @label_calendar = add UILabel, :label_calendar do
+      frame [[80, -100],['100%','100%']]
+      text "5:00 PM"
+      text_color UIColor.blackColor
+      text_alignment UITextAlignmentCenter
+    end
+    @label_calendar.center = CGPointMake(80, 160)
+
+    @image_runner = UIImageView.alloc.initWithImage(UIImage.imageNamed("running30.png"))
+    addSubview(@image_runner)
+    @image_runner.center = CGPointMake(160, 130)
+
+    @label_runner = add UILabel, :label_runner do
+      frame [[160, -100],['100%','100%']]
+      text "5.5 mi"
+      text_color UIColor.blackColor
+      text_alignment UITextAlignmentCenter
+    end
+    @label_runner.center = CGPointMake(160, 160)
+
+    @image_timer = UIImageView.alloc.initWithImage(UIImage.imageNamed("chronograph1.png"))
+    addSubview(@image_timer)
+    @image_timer.center = CGPointMake(240, 130)
+
+    @label_timer = add UILabel, :label_timer do
+      frame [[240, -100],['100%','100%']]
+      text "9:00 min/mi"
+      text_alignment UITextAlignmentCenter
+    end
+    @label_timer.center = CGPointMake(240, 160)
+
+    background_color UIColor.grayColor
   end
 end
