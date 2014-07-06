304e0b3594a7205d273576b26aa033ea921ef2f4
~/.gitconfig <benmiller179@gmail.com>
2014-07-06 15:39:57 -0400

    Make app_delegate normal again

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -7,8 +7,7 @@ class AppDelegate
 
     @main_controller = MainController.alloc.initWithNibName(nil, bundle: nil)
     @invite_controller = InviteController.alloc.initWithNibName(nil, bundle: nil)
-    #@nav_controller = UINavigationController.alloc.initWithRootViewController(@main_controller)
-    @nav_controller = UINavigationController.alloc.initWithRootViewController(@invite_controller)
+    @nav_controller = UINavigationController.alloc.initWithRootViewController(@main_controller)
     @nav_controller.setDelegate(self)
     @window.rootViewController = nav_controller
 
